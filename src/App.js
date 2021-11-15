3ec40e818dc4886570c6ec7451c9777d2689e4b1
Sebastian Campos <sebastian.campos321@gmail.com>
2021-11-14 23:34:35 -0500

    file structure with navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from "./components/Navbar"
 function App() {
   return (
-   <h2>Portfolio</h2>
+   <Navbar />
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,27 @@
 import React from 'react'
-
+import logo from '../logo.jpg'
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Hello</a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+     <div className="container">
+    <a className="navbar-brand" href="/"><img src={logo} alt="logo..."/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="/">Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/">Link</a>
         </li>
       </ul>
+      
     </div>
-  </div>
-</nav>
+    </div>
+  </nav>
     )
 }
 
